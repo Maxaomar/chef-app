af39e47233ba9265e24d5eacd4986c6e06661db8
MOHAMED <ahmadiin612@gmail.com>
2025-01-06 10:48:16 +0300

    added startup receipe chef app

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default  function Header() {
+    return (
+        <header>
+            <img src="src/images/chef.png" alt="chef image" />
+            <h1>Chef Mster</h1>
+        </header>
+    )
+}
