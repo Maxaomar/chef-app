b12af4636c3c82c1133588a67211ccdb908a3de5
MOHAMED <ahmadiin612@gmail.com>
2025-01-09 22:21:11 +0300

    modified Main.jsx file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,30 @@
+import React from "react";
+
 export default  function Main() {
+    const [ingredients, setIngredients] = React.useState([]);
+
+    const listOfIngredients = ingredients.map((ingredient) => (
+               <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+
     return (
         <main>
-            <form>
-                <input  type="text" placeholder="e.g oregano" aria-label="add ingredient"/>
+            <form onSubmit={handleSubmit}>
+                <input  type="text" placeholder="e.g oregano" aria-label="add ingredient" name="ingredient"/>
                    <button>Add Ingredient</button>
             </form>
+            <ul>
+                {listOfIngredients}
+            </ul>
         </main>
     )
 }
