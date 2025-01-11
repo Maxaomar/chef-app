4b4d2dd4eb2f1c417bb28f7cb3dc53aaf63d303b
MOHAMED <ahmadiin612@gmail.com>
2025-01-11 23:32:08 +0300

    updated the form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+
 export default function Main() {
 
     const [ingredients, setIngredients] = React.useState([])
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client"
 import App from "./App"
+import ReactDom from "react-dom/client"
 
 
 
