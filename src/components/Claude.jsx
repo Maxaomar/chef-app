37b7b8f3040268e6bd4e2f2538d62de9e90a0144
MOHAMED <ahmadiin612@gmail.com>
2025-01-17 20:52:06 +0300

    added new file

--- src/components/Claude.jsx
+++ src/components/Claude.jsx
@@ -1,50 +1,6 @@
-import React from "react"
-
-
-export default function Main() {
-
-    const [ingredients, setIngredients] = React.useState(["all the main spices", "pasta", "ground beef", "tomato paste"])
-
-    const [recipeShown, setRecipeShown] = React.useState(false)
-
-
-    function toggleRecipeShown() {
-        setRecipeShown(prevShown => !prevShown)
-    }
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
-
-    function addIngredient(formData) {
-        const newIngredient = formData.get("ingredient")
-        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
-    }
-
-    return (
-        <main>
-            <form action={addIngredient} className="add-ingredient-form">
-                <input
-                    type="text"
-                    placeholder="e.g. oregano"
-                    aria-label="Add ingredient"
-                    name="ingredient"
-                />
-                <button>Add ingredient</button>
-            </form>
-
-            {ingredients.length > 0 && <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                {ingredients.length > 3 && <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button onClick={toggleRecipeShown}>Get a recipe</button>
-                </div> }
-            </section>}
-
-            {recipeShown && <section>
+export default function Claude() {
+    return(
+        <section>
                 <h2>Chef Claude Recommends:</h2>
                 <article className="suggested-recipe-container" aria-live="polite">
                     <p>Based on the ingredients you have available, I would recommend making a simple a delicious <strong>Beef Bolognese Pasta</strong>. Here is the recipe:</p>
@@ -76,7 +32,5 @@ export default function Main() {
                     </ol>
                 </article>
             </section>
-            }
-        </main>
     )
 }
